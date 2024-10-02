d0d2cd04b2305a9f1cdbf4e47bac989b85880e7c
gl-work-curs <work@webr3.org>
2024-10-02 19:34:26 +0100

    nix scrollspy, it's junk

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,6 @@ module.exports = function (grunt) {
           'js/alert.js',
           'js/button.js',
           'js/collapse.js',
-          'js/scrollspy.js',
           'js/modal.js',
           'js/affix.js'
         ],
